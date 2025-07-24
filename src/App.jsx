6f989cafdbef06dec14ff158e64c71666b9fc761
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-24 14:17:29 +0200

    Fetch job data from API and manage loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "https://www.course-api.com/react-tabs-project";
+
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchJobs = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios(url);
+      setJobs(res.data);
+    } catch (error) {
+      throw new Error(`Something went wrong ${error}`);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="job-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return (
     <>
       <h2>hello comapines</h2>
